10b8517d0b87e799cdaa9965fc977a32fd38f9e8
Russo <erkmann08@gmail.com>
2019-04-09 14:07:16 -0300

    operacoes matematicas

---
+++ operacoesMatematicas.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+
+    //Definindo variaveis
+    int a = 5, b = 3;
+
+    printf("\nA soma de %d e %d = %d", a, b, a + b);
+
+    printf("\nA subtracao de %d e %d = %d", a, b, a - b);
+
+    printf("\nA divisao de %d e %d = %d", a, b, a / b);
+
+    printf("\nA multiplicacao de %d e %d = %d", a, b, a * b);
+
+    printf("\nO resto da divisao de %d e %d = %d", a, b, a % b);
+
+    printf("\nO valor absoluto de %d = %d", a - b, abs(a - b));
+
+}
