8fefea20d9c8eff9d9063cba3be2067653c10b21
Russo <erkmann08@gmail.com>
2019-04-11 13:29:13 -0300

    condicionaisBooleanas

---
+++ condicioanaisBooleanas.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void main(){
+
+    bool a = true, b = false;
+
+    if (a){
+        printf("\n%d eh igual verdadeiro", a);
+    }
+
+    if (!b){
+        printf("\n%d eh igual falso", b);
+    }
+
+}
