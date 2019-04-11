f10848fa1aa01b7989d0c5dcb3bf8e5029e72582
Russo <erkmann08@gmail.com>
2019-04-11 13:33:53 -0300

    condicionaisComChar

---
+++ condicionaisComChar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void main(){
+
+    char letra = 'x';
+
+    if(letra == 'x'){
+        printf("\n A letra eh x");
+        printf("\n o codigo da letra eh %d", letra);
+    }
+
+
+
+}
