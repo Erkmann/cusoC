0f7df5dbc08c052cf6a00cb6d4d6c5fddbd5624c
Russo <erkmann08@gmail.com>
2019-04-12 13:16:51 -0300

    while

---
+++ lacosRepeticao.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+
+    int a = 1, b = 10;
+
+    while(a <= 10){
+        printf("\n %d", a);
+        a++;
+    }
+
+    printf("\n Regresiva");
+
+    while(b >= 1){
+        printf("\n %d", b);
+        b--;
+    }
+
+}
