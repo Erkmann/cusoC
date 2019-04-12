1ffb74f24b43d30ddbd139bb7f11551b6891e490
Russo <erkmann08@gmail.com>
2019-04-12 13:21:29 -0300

    doWhile

---
+++ doWhile.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+
+    int a = 1, b = 10;
+
+    printf("\n Regresiva");
+
+    do{
+        printf("\n %d", b);
+        b--;
+    }while(b >= 1);
+
+}
